ca3b8cafb9d63ee4ac71902c2cd1035d0ed024c9
jakobpederson <jakob.pederson+github@gmail.com>
2018-03-18 11:06:46 -0500

    first pass

--- exercise_8.py
+++ exercise_8.py
@@ -11,4 +11,4 @@ bag,hello,without,world
 '''
 
 def get_sorted_list(words):
-    pass
+    return sorted(words)
--- test_exercise_8.py
+++ test_exercise_8.py
@@ -6,4 +6,11 @@ from exercise_8 import get_sorted_list
 class Exercise8Test(TestCase):
 
     def test_x(self):
-        self.fail('x')
+        words = [
+            'without', 'hello', 'bag', 'world'
+        ]
+        result = get_sorted_list(words)
+        expected = [
+            'bag', 'hello', 'without', 'world'
+        ]
+        self.assertEqual(result, expected)
