45880ed8db0c16688aa52144d459d8ffffd3b7f5
jakobpederson <jakob.pederson+github@gmail.com>
2018-03-18 11:00:27 -0500

    Solved

--- test_exercise_7.py
+++ test_exercise_7.py
@@ -4,7 +4,7 @@ from exercise_7 import get_2_dimensional_array
 
 class Exercise7Test(TestCase):
 
-    def test_x(self):
+    def test_example(self):
         expected = [
             [0, 0, 0, 0, 0],
             [0, 1, 2, 3, 4],
@@ -12,8 +12,18 @@ class Exercise7Test(TestCase):
         ]
         self.assertCountEqual(get_2_dimensional_array(3, 5), expected)
 
-    def test_y(self):
+    def test_1_2(self):
         expected = [
             [0, 0],
         ]
         self.assertCountEqual(get_2_dimensional_array(1, 2), expected)
+
+    def test_4_2(self):
+        expected = [
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [0, 3],
+        ]
+        self.assertCountEqual(get_2_dimensional_array(4, 2), expected)
+
