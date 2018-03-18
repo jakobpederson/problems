d7194df1128ed4d8405a1769765558f9cd44badb
jakobpederson <jakob.pederson+github@gmail.com>
2018-03-18 10:52:48 -0500

    first failure

--- test_exercise_7.py
+++ test_exercise_7.py
@@ -1,7 +1,13 @@
 from unittest import TestCase
 
+from exercise_7 import get_2_dimensional_array
 
 class Exercise7Test(TestCase):
 
     def test_x(self):
-        self.fail('x')
+        expected = [
+            [0, 0, 0, 0, 0],
+            [0, 1, 2, 3, 4],
+            [0, 2, 4, 6, 8]
+        ]
+        self.assertCountEqual(get_2_dimensional_array(3, 5), expected)
